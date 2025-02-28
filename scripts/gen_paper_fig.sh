8af5fa6485deaaf46fca471625aeab3013f6e0b5
David Baldassin <baldassin@i3s.unice.fr>
2025-02-28 12:00:35 +0100

    add script to generate all fiigures for the paper

---
+++ scripts/gen_paper_fig.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+function move_files() {
+    SRC=$1
+    DEST=$2
+
+    mv $SRC/*2_1.pdf $DEST/2_1
+    mv $SRC/*1_1.pdf $DEST/1_1
+}
+
+
+# create file tree
+
+# progressive reduction
+mkdir -p figures/progressive-reduction/ballooning/2_1
+mkdir -p figures/progressive-reduction/ballooning/1_1
+mkdir -p figures/progressive-reduction/cgroups/2_1
+mkdir -p figures/progressive-reduction/cgroups/1_1
+mkdir -p figures/progressive-reduction/all/2_1
+mkdir -p figures/progressive-reduction/all/1_1
+
+# one step closer
+mkdir -p figures/steps/2_1
+mkdir -p figures/steps/1_1
+
+# pid regulation
+mkdir -p figures/regulation/ballooning/2_1
+mkdir -p figures/regulation/ballooning/1_1
+
+# viewer burst
+mkdir -p figures/viewer-burst/ballooning/2_1
+mkdir -p figures/viewer-burst/ballooning/1_1
+mkdir -p figures/viewer-burst/cgroups/2_1
+mkdir -p figures/viewer-burst/cgroups/1_1
+mkdir -p figures/viewer-burst/all/2_1
+mkdir -p figures/viewer-burst/all/1_1
+
+# save path
+root_wd=$PWD
+plot_exe=$root_wd/scripts/plot.py
+boxplot_exe=$root_wd/scripts/line-step.py
+
+# start generate pics
+# progressive reduction
+
+cd $root_wd/results/ok/reduction-stable
+
+balloon_avg_file=ballooning-simulcast-vp8/ballooning-simulcast-vp8_2025-02-04-13-37-49_average_20.csv
+reclaim_avg_file=cgroups-reclaim/cgroups-reclaim_2025-02-19-23-59-01_average_20.csv
+max_avg_file=cgroups-max-reduction-async/cgroups-max-reduction-simulcast-vp8_2025-02-04-13-37-50_average_20.csv
+
+$plot_exe $balloon_avg_file avg TIME VIRSH_AVAILABLE,VIRSH_USABLE,VIRSH_SWAP_OUT,MEMORY_USED MEMORY_PRESSURE_AVG10
+move_files ballooning-simulcast-vp8 $root_wd/figures/progressive-reduction/ballooning
+
+$plot_exe $balloon_avg_file,$max_avg_file,$reclaim_avg_file avg TIME PUBLISHER_BITRATE PUBLISHER_RTT
+move_files . $root_wd/figures/progressive-reduction/all
+
+$plot_exe $max_avg_file,$reclaim_avg_file avg TIME MEMORY_USED,MEMORY_MAX,SWAP MEMORY_PRESSURE_AVG10
+move_files . $root_wd/figures/progressive-reduction/cgroups
+
+# one step closer
+cd $root_wd/results/ok/one-step-closer
+
+$boxplot_exe cgroups-max cgroups-reclaim ballooning
+move_files . $root_wd/figures/steps
+
+# viewer burst
+cd $root_wd/results/ok/viewer-burst-no-increase
+
+balloon_avg_file=ballooning/ballooning-lowthresh-viewers70-thresh50-increase0/ballooning-lowthresh-viewers70-thresh50-increase0_2025-02-13-13-45-34_average_5.csv
+reclaim_avg_file=cgroups-reclaim/cgroups-reclaim-viewersburst70-thresh50-increase0/cgroups-reclaim-viewersburst70-thresh50-increase0_2025-02-18-15-43-31_average_5.csv
+max_avg_file=cgroups-max/cgroups-max-reduction-viewers70-thresh50-increase0/cgroups-max-reduction-viewers70-thresh50-increase0_2025-02-18-15-43-20_average_5.csv
+
+balloon_dir=ballooning/ballooning-lowthresh-viewers70-thresh50-increase0
+reclaim_dir=cgroups-reclaim/cgroups-reclaim-viewersburst70-thresh50-increase0
+max_dir=cgroups-max/cgroups-max-reduction-viewers70-thresh50-increase0
+
+$plot_exe $balloon_avg_file avg TIME VIRSH_AVAILABLE,VIRSH_SWAP_OUT,VM_MEMORY_USAGE [320,450]
+move_files $balloon_dir $root_wd/figures/viewer-burst/ballooning
+
+$plot_exe $max_avg_file avg TIME MEMORY_USED,MEMORY_MAX,SWAP,VM_MEMORY_USAGE MEMORY_PRESSURE_AVG10 [320,450]
+move_files $max_dir $root_wd/figures/viewer-burst/cgroups
+
+$plot_exe $balloon_avg_file,$max_avg_file,$reclaim_avg_file avg TIME PUBLISHER_BITRATE PUBLISHER_RTT [320,450]
+move_files ballooning $root_wd/figures/viewer-burst/all
+
