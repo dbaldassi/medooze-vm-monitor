61f32a36d0c2176cbc710d41e50add8f713f46f7
David Baldassin <david.baldassin@cosmosoftware.io>
2025-10-07 16:01:39 +0200

    add multiroom scenar

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REPET=10
+REPET=5
 # SCENARIO=("reduction-viewers" "reclaim-reduction-viewers")
 # SCENARIO=("visio/visio_perf_maxroom" "visio/visio_reclaim_regul" "visio/visio_balloon_regul")
 # SCENARIO=("visio/visio_balloon_regul")
-SCENARIO=("visio/visio_perf_maxroom")
+SCENARIO=("visio/visio_perf_maxroom" "visio/visio_multiroom_baseline" "visio/visio_multiroom_balloon" "visio/visio_multiroom_cgroups")
 # SCENARIO=("cgroup-reclaim-step")
 # SCENARIO=("spawn-cgroup-reclaim" "spawn-cgroup-max" "spawn-balloon")
 # SCENARIO=("max2500")
@@ -249,7 +249,7 @@ trap 'trap_sigint' INT
 REPET=10
 # SCENARIO=("reduction" "reclaim-reduction")
 run
-run_pid_compare
+# run_pid_compare
 
 # REPET=5
 # SCENARIO=("reduction-viewers" "reclaim-reduction-viewers")
